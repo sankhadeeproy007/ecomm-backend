1f6fad859f344cd16d2bd11ab67397e7b6069aac
Sankhadeep Roy <sankhadeeproy007@gmail.com>
2019-12-08 19:18:04 +0530

    chore(): Adds files to new repo :tada:

---
+++ src/index.js
@@ -0,0 +1,22 @@
+require('dotenv').config({ path: 'variables.env' });
+const createServer = require('./createServer');
+const db = require('./db');
+
+const server = createServer();
+
+server.express.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Credentials', true);
+  next();
+});
+
+server.start(
+  {
+    cors: {
+      credentails: true,
+      origin: process.env.FRONTEND_URL
+    }
+  },
+  deets => {
+    console.log(`Server is now running on http:/localhost:${deets.port}`);
+  }
+);
