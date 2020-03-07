f08aa68e2ad49186be4fb2582e7aeb58b49d33b7
Sankhadeep Roy <sankhadeeproy007@gmail.com>
2020-03-07 22:04:44 +0530

    feat(): adds delete functionality in the backend :sparkles:

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -2,28 +2,24 @@ const Mutations = {
   async createItem(parent, args, ctx, info) {
     // TODO: Check if they are logged in
 
-    const item = await ctx.db.mutation.createItem(
-      {
-        data: {
-          ...args
-        }
-      },
-      info
-    );
-
-    return item;
+    return ctx.db.mutation.createItem({ data: { ...args } }, info);
   },
   updateItem(parent, args, ctx, info) {
     const updates = { ...args };
     delete updates.id;
 
     return ctx.db.mutation.updateItem(
-      {
-        data: updates,
-        where: { id: args.id }
-      },
+      { data: updates, where: { id: args.id } },
       info
     );
+  },
+  async deleteItem(parent, args, ctx, info) {
+    const where = { id: args.id };
+    // 1. Find the item
+    const item = await ctx.db.query.item({ where }, `{ id title }`);
+    // 2. Check if they own the item or have permissions
+    // 3. Delete it
+    return ctx.db.mutation.deleteItem({ where }, info);
   }
 };
 
